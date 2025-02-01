12bf33099ae7130978ea0d79f9d3e0b6ca0f9533
justinmassiot <justinmassiot@users.noreply.github.com>
2025-02-01 13:39:37 +0100

    Clarifying how WiFi credentials work

--- coco_bedtime_clock/wifi_info.h
+++ coco_bedtime_clock/wifi_info.h
@@ -1,2 +1,7 @@
-#define SECRET_SSID "[[SSID]]"
-#define SECRET_PASS "[[PASS]]"
+// Please set your WiFi credentials below
+
+// your network SSID (name)
+#define SECRET_SSID "[[YOUR_WIFI_SSID]]"
+
+// your network password (use for WPA, or use as key for WEP)
+#define SECRET_PASS "[[YOUR_WIFI_PASS]]"
