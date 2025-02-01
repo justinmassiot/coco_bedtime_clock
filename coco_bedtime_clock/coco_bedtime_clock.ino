54b2ec0ebda6cd7da4c67c1c85fc25bdc98e7655
justinmassiot <justinmassiot@users.noreply.github.com>
2025-02-01 15:40:14 +0100

    Better WiFI / NTP / RTC error handling

--- coco_bedtime_clock/coco_bedtime_clock.ino
+++ coco_bedtime_clock/coco_bedtime_clock.ino
@@ -8,6 +8,8 @@ Inspired by the "RTC_NTPSync" and "DisplaySingleFrame" examples.
 LED matrix pictures and animations can be built at: https://ledmatrix-editor.arduino.cc/
 */
 
+#define DEBUG_COCO 1
+
 #include <WiFiS3.h>
 #include <WiFiUdp.h>
 #include <NTPClient.h> // needs the NTPClient library to be installed
@@ -27,56 +29,64 @@ ArduinoLEDMatrix LedMatrix;
 bool flag_update = false;
 
 
-bool connectToWiFi(unsigned int waiting_time_ms) {
-  int wifi_status = WL_IDLE_STATUS;
+// attempt to connect to WiFi network
+bool connectToWiFi() {
+  if (DEBUG_COCO) {
+    Serial.print("[WIFI] Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    Serial.flush();
+  }
 
-  // attempt to connect to WiFi network
-  Serial.print("[WIFI] Attempting to connect to SSID: ");
-  Serial.println(ssid);
-  wifi_status = WiFi.begin(ssid, pass); // connect to WPA/WPA2 network; change this line if using open or WEP network
-
-  // TODO: loop on status until timeout
-  delay(waiting_time_ms);
-
-  return (wifi_status == WL_CONNECTED); // true if connected, false otherwise
+  // change the below line if using Open (unsecured) or WEP network
+  return (WiFi.begin(ssid, pass) == WL_CONNECTED); // true if connected, false otherwise
+                                                   // note: this routine is blocking and makes several attempts based on WL_MAX_ATTEMPT_CONNECTION
 }
 
 void printWifiStatus() {
-  // print the SSID of the network you're attached to:
-  Serial.print("[WIFI] SSID: ");
-  Serial.println(WiFi.SSID());
+  if (DEBUG_COCO) {
+    // print the SSID of the network you're attached to:
+    Serial.print("[WIFI] SSID: ");
+    Serial.println(WiFi.SSID());
 
-  // print your board's IP address:
-  Serial.print("[WIFI] IP Address: ");
-  Serial.println(WiFi.localIP());
+    // print your board's IP address:
+    Serial.print("[WIFI] IP Address: ");
+    Serial.println(WiFi.localIP());
 
-  // print the received signal strength:
-  Serial.print("[WIFI] Signal strength (RSSI):");
-  Serial.print(WiFi.RSSI());
-  Serial.println(" dBm");
+    // print the received signal strength:
+    Serial.print("[WIFI] Signal strength (RSSI): ");
+    Serial.print(WiFi.RSSI());
+    Serial.println(" dBm");
+  }
 }
 
-void setRtcFromNtp() {
-  Serial.println("[NTP] Getting time from NTP and copying it to RTC...");
+// get the current date and time from NTP and push it to the RTC
+bool setRtcFromNtp() {
+  bool ntp_time_ok = false;
+  bool rtc_time_ok = false;
+
+  if (DEBUG_COCO) Serial.println("[NTP]  Getting time from NTP and copying it to RTC...");
   
-  // get the current date and time from NTP
-  NtpClient.begin();
-  NtpClient.update();
-  auto timezone_offset_hours = 0;
-  auto unix_time = NtpClient.getEpochTime() + (timezone_offset_hours * 3600);
-  Serial.print("[NTP] Unix time = ");
-  Serial.println(unix_time);
-  RTCTime time_to_set = RTCTime(unix_time);
-  RTC.setTime(time_to_set);
+  NtpClient.begin(); // starts the underlying UDP client with the default local port
+  ntp_time_ok = NtpClient.forceUpdate(); // has in internal timeout of 1000ms
+  if (ntp_time_ok) {
+    auto timezone_offset_hours = 0;
+    auto unix_time = NtpClient.getEpochTime() + (timezone_offset_hours * 3600);
+    if (DEBUG_COCO) Serial.print("[NTP]  Unix time = ");
+    Serial.println(unix_time);
 
-  // TODO: set timezone and DST with https://github.com/JChristensen/Timezone/blob/master/examples/HardwareRTC/HardwareRTC.ino
+    // TODO: set timezone and DST
+    // https://github.com/arduino-libraries/NTPClient/blob/master/NTPClient.h#L92C10-L92C40
+    // https://github.com/JChristensen/Timezone/blob/master/examples/HardwareRTC/HardwareRTC.ino
 
+    RTCTime time_to_set = RTCTime(unix_time);
+    rtc_time_ok = RTC.setTime(time_to_set);
+  }
   NtpClient.end();
+
+  return (ntp_time_ok && rtc_time_ok);
 }
 
 void setup() {
-  bool is_wifi_connected;
-
   Serial.begin(115200);
   delay(1000); // wait for the Serial to really be ready
   Serial.println("==========");
@@ -87,20 +97,22 @@ void setup() {
   LedMatrix.begin();
 
   Serial.println("INITIALIZING...");
+
+  // TODO: in case of permanent error, show it on the screen
   
   // initialize external WiFi module
+  Serial.println("[INIT] WiFi module...");
   if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    Serial.println("STOP");
-    for(;;){} // stop definitely
+    Serial.println("[ERROR] Communication with WiFi module failed!");
+    for(;;){} // can't go further
   }
   Serial.println("[INIT] WiFi module OK");
 
   // initialize the RTC
+  Serial.println("[INIT] Real-time Clock...");
   if (!RTC.begin()) {
-    Serial.println("RTC init failed!");
-    Serial.println("STOP");
-    for(;;){} // stop definitely
+    Serial.println("[ERROR] RTC init failed!");
+    for(;;){} // can't go further
   }
   Serial.println("[INIT] Real-time Clock OK");
   
@@ -108,32 +120,34 @@ void setup() {
     LedMatrix.loadSequence(LEDMATRIX_ANIMATION_WIFI_SEARCH); // https://docs.arduino.cc/tutorials/uno-r4-wifi/led-matrix/#frame-gallery
     LedMatrix.play(true);
 
-    do {
-      is_wifi_connected = connectToWiFi(10000);
+    if (!connectToWiFi()) {
+      Serial.println("[ERROR] Can't connect to WiFi!");
+      for(;;){} // can't go further
     }
-    while (!is_wifi_connected); // infinite loop until connection
-    Serial.println("[WIFI] Successfully connected to WiFi");
-    //printWifiStatus();
-    setRtcFromNtp();
+    // else:
+    if (DEBUG_COCO) Serial.println("[WIFI] Successfully connected to WiFi");
+    printWifiStatus();
+
+    if (!setRtcFromNtp()) {
+      Serial.println("[ERROR] Can't set time to RTC!");
+      for(;;){} // can't go further
+    }
+    // else:
+    if (DEBUG_COCO) Serial.println("[NTP]  Successfully set RTC from NTP time");
 
     LedMatrix.play(false);
     LedMatrix.clear();
   }
   Serial.println("[INIT] The RTC should now be running.");
 
-  /*
-  if (!RTC.setPeriodicCallback(periodicCallback, Period::ONCE_EVERY_2_SEC)) { // https://docs.arduino.cc/tutorials/uno-r4-minima/rtc/#periodic-interrupt
-    Serial.println("[ERROR] Periodic callback not set");
-  }
-  */
-
   // set a callback every day at 3:00 for clock update
   RTCTime UpdateAlarmTime;
   AlarmMatch UpdateMatchTime;
-  UpdateAlarmTime.setHour(3); // match when hour = 3
+  UpdateAlarmTime.setHour(3); // match when hour = 3 // TODO: apply timezone setting?
   UpdateMatchTime.addMatchHour();
   if (!RTC.setAlarmCallback(rtcUpdateCallback, UpdateAlarmTime, UpdateMatchTime)) { // https://docs.arduino.cc/tutorials/uno-r4-minima/rtc/#alarm-callback
-    Serial.println("[ERROR] Update callback not set");
+    Serial.println("[ERROR] Time update callback can't be set!");
+    for(;;){} // can't go further
   }
 }
 
@@ -149,8 +163,8 @@ void loop() {
   if (flag_update) {
     digitalWrite(LED_BUILTIN, HIGH);
     Serial.println("[RTC] Waking up for datetime update");
-    if (connectToWiFi(10000)) {
-      Serial.println("[NTP] Successfully connected to WiFi");
+    if (connectToWiFi()) {
+      if (DEBUG_COCO) Serial.println("[NTP]  Successfully connected to WiFi");
       setRtcFromNtp();
     }
     WiFi.end(); // we don't need to stay connected
@@ -159,27 +173,27 @@ void loop() {
   }
 
   // time comparison
-  Serial.println("---");
+  if (DEBUG_COCO) Serial.println("---");
   RTC.getTime(CurrentTime);
-  //Serial.println(DayOfWeek2int(CurrentTime.getDayOfWeek(), false));
-  Serial.print(CurrentTime.getUnixTime());
-  Serial.println(" / "+ String(CurrentTime));
+  //if (DEBUG_COCO) Serial.println(DayOfWeek2int(CurrentTime.getDayOfWeek(), false));
+  if (DEBUG_COCO) Serial.print(CurrentTime.getUnixTime());
+  if (DEBUG_COCO) Serial.println(" / "+ String(CurrentTime));
   CurrentTime.setDayOfMonth(1);
   CurrentTime.setMonthOfYear(Month::JANUARY);
   CurrentTime.setYear(1970);
-  Serial.print(CurrentTime.getUnixTime());
-  Serial.println(" / "+ String(CurrentTime));
+  if (DEBUG_COCO) Serial.print(CurrentTime.getUnixTime());
+  if (DEBUG_COCO) Serial.println(" / "+ String(CurrentTime));
 
-  Serial.print(wakeup.getUnixTime());
-  Serial.println(" / "+ String(wakeup));
-  Serial.print(gosleep.getUnixTime());
-  Serial.println(" / "+ String(gosleep));
+  if (DEBUG_COCO) Serial.print(wakeup.getUnixTime());
+  if (DEBUG_COCO) Serial.println(" / "+ String(wakeup));
+  if (DEBUG_COCO) Serial.print(gosleep.getUnixTime());
+  if (DEBUG_COCO) Serial.println(" / "+ String(gosleep));
   
   if ((!is_day_set || is_day_up) && (CurrentTime.getUnixTime() < wakeup.getUnixTime() || CurrentTime.getUnixTime() >= gosleep.getUnixTime())) {
     // day -> night transition
     is_day_set = true;
     is_day_up = false;
-    Serial.println("It's time to sleep");
+    if (DEBUG_COCO) Serial.println("It's time to sleep");
     LedMatrix.loadSequence(anim_asleep);
     LedMatrix.play(true);
   }
@@ -187,7 +201,7 @@ void loop() {
     // night -> day transition
     is_day_set = true;
     is_day_up = true;
-    Serial.println("It's time to wake up");
+    if (DEBUG_COCO) Serial.println("It's time to wake up");
     LedMatrix.loadSequence(anim_awake);
     LedMatrix.play(true);
   }
